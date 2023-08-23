fe06a79468f6572744e3a8d25236b149bdc2e118
femmytedrey <femmytedrey@gmail.com>
2023-08-23 09:37:23 +0100

    javascript in progress

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 const button = document.getElementById("addButton");
 
+
+
 button.addEventListener('click', () => {
     if(inputBox.value === ''){
         alert('Input cannot be empty!');
@@ -14,12 +16,24 @@ button.addEventListener('click', () => {
         li.appendChild(span);
     }
     inputBox.value = '';
+    saveData();
 });
 
 listContainer.addEventListener('click', (e) =>{
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        saveData();
     }else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
 }, false);
+
+const saveData = () =>{
+    try {
+        localStorage.setItem("data", listContainer.innerHTML);
+        console.log("Data saved successfully!");
+      } catch (error) {
+        console.error("Error while saving data:", error);
+      }
+};
